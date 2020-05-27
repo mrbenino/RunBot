8a6fe886cc7dacdacf0e7b1e1781f98cf39b501d
Benin Benino <mrbenino@MacBook-Pro.local>
2020-05-27 14:38:11 +0300

    delete not need scenes, add regex for ms

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
 const Telegraf = require('telegraf')
 const config = require('config')
 const bot = new Telegraf(config.get('token'))
-const {
-    Extra,
-    Markup,
-    Stage,
-    session
-  } = Telegraf
-const ScenesGenerator = require('./Scenes')
-const currScene = new ScenesGenerator()
-const ageScene = currScene.GenAgeScene()
-const nameScene = currScene.GenNameScene()
-const runScene = currScene.GenRunScene()
-const nextScene = currScene.GenNextScene()
-const stage = new Stage([ageScene, nameScene, runScene, nextScene])
+const regex = /^(бег [0-9]{1,2})$/i
 
-bot.use(Telegraf.log())
-bot.use(session())
-bot.use(stage.middleware())
+console.log('┌────────────────────┐')
+console.log('│ bot by:  @mrbenino │')
+console.log('└────────────────────┘')
+// bot.use(Telegraf.log())
+
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.command('echo', (ctx) => ctx.reply('Echo'))
-bot.command('scenes', async (ctx) => {
-    ctx.scene.enter('age')
-})
 bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+bot.on('text', (ctx) => {
+    let ms = ctx.message.text
+    if (regex.test(ms)) {
+        console.log('ok')
+        ctx.reply('👍')
+    } else {
+        console.log('not ok')
+    }
+})
+
+
 bot.launch() 
