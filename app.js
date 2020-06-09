dc085d6dbd0dc435355c27a9347eccc26c2039dc
Benin Benino <mrbenino@MacBook-Pro-2.local>
2020-06-09 15:58:43 +0300

    stabil v1.0

--- app.js
+++ app.js
@@ -32,46 +32,13 @@ bot.on('text', (ctx) => {
             data: 'username='+ctx.message.from.username+'&result='+ text
         })
         .then(function (response) {
-            // console.table(response.data)
-            let today = new Date()
-            let diff = today.getDay
-            // console.log(today.toDateString())
-            let temp_day = new Date()
-            let actual_result = 0
-            let month_resualt = 0
-            let month_dump = 0
-            let week_result = 0
-            let dump = 0
-            let yor_dump = 0
-            
+            console.table(response.data)
             for (let i = 0; i < response.data.length; i++) {
-                dump += Number(response.data[i].result)
-                temp_day.setTime(Date.parse(response.data[i].receipt_date))
-                if(response.data[i].username == ctx.message.from.username){
-                    yor_dump += Number(response.data[i].result)
-                    if(temp_day.getFullYear == today.getFullYear && temp_day.getMonth == today.getMonth){
-                        month_resualt += Number(response.data[i].result)
-                    }
-                }
-                if(temp_day.getFullYear == today.getFullYear && temp_day.getMonth == today.getMonth){
-                    month_dump += Number(response.data[i].result)
-                }
-                if (temp_day.getDate == today.getDate) {
-                    actual_result += Number(response.data[i].result)
-                }
+                response.data[i].total = Number(response.data[i].total)
             }
             console.table(response.data)
-            for (let i = response.data.length - 1; i ; i--) {
-                console.log(response.data[i].receipt_date)
-                temp_day.setTime(Date.parse(response.data[i].receipt_date))
-                if(temp_day.getDate == today.getDate){
-                    week_result += Number(response.data[i].result)
-                }else if(temp_day.getDay < today.getDay && temp_day.getDay != 0) {
-                    week_result += Number(response.data[i].result)
-                }else {break;}
-            }
 
-            ctx.reply(`───Статистика ${ctx.message.from.first_name} 👟───\n${ctx.message.from.first_name} Ваш результат 👟 сегодня ${actual_result.toFixed(2)} km \n${ctx.message.from.first_name} результат за неделю 👟 ${week_result.toFixed(2)} km\n${ctx.message.from.first_name} результат за месяц 👟 ${month_resualt.toFixed(2)} km\n${ctx.message.from.first_name} общий результат 👟 ${yor_dump.toFixed(2)} km\n───Статистика группы 🌓🏃🏾‍♀️───\nЗа месяц 🏃‍♀️🏃🏻‍♂️🏃🏾‍♀️ ${month_dump.toFixed(2)} km до 🌓\nВсего 🏃пробежали до 🌓 ${dump.toFixed(2)} km\nНам осталось до 🌓 ${300000 - dump.toFixed(2)} km\n`)
+            ctx.reply(`───Статистика ${ctx.message.from.first_name} 👟───\n${ctx.message.from.first_name} Ваш результат 👟 сегодня ${response.data[0].total.toFixed(2)} km \n${ctx.message.from.first_name} результат за неделю 👟 ${response.data[1].total.toFixed(2)} km\n${ctx.message.from.first_name} результат за месяц 👟 ${response.data[2].total.toFixed(2)} km\n───Статистика группы 🌓🏃🏾‍♀️───\nЗа месяц 🏃‍♀️🏃🏻‍♂️🏃🏾‍♀️ ${response.data[5].total.toFixed(2)} km до 🌓\nВсего 🏃пробежали до 🌓 ${response.data[6].total.toFixed(2)} km\nНам осталось до 🌓 ${300000 - response.data[6].total.toFixed(2)} km\n`)
         })
         .catch(function (error) {
             console.log(error);
